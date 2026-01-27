7e396d5e0991b0efef8f5aeb3009538e885df411
axelnygren <s.axel.nygren@gmail.com>
2026-01-27 11:13:36 +0100

    hejhej

--- Car.java
+++ Car.java
@@ -1,3 +1,65 @@
+import java.awt.*;
+
 public abstract class Car {
 
+    // INSTANSVARIABLER
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+
+
+    //KONSTRUKTOR
+    public Car() {
+    }
+
+    // METODER
+    // Get/Set-Metoder
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    // Start/Stop - Engine
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    //Abstrakta metoder
+    public abstract double speedFactor();
+
+    public abstract void incrementSpeed(double amount);
+
+    public abstract void decrementSpeed(double amount);
+
+
+    //Gas/break
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
 }
--- Saab95.java
+++ Saab95.java
@@ -1,15 +1,12 @@
 import java.awt.*;
 
-public class Saab95{
+public class Saab95 extends Car{
 
     public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
+
+    // KONSTRUKTOR
     public Saab95(){
+        super();
         nrDoors = 2;
         color = Color.red;
         enginePower = 125;
@@ -17,34 +14,8 @@ public class Saab95{
         modelName = "Saab95";
         stopEngine();
     }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-
+    // METODER
     public void setTurboOn(){
 	    turboOn = true;
     }
@@ -65,15 +36,5 @@ public class Saab95{
 
     public void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-    }
-    
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
     }
 }
