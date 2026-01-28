972ac69da842c1c5b2e92dde7c6406b9cde49141
axelnygren <s.axel.nygren@gmail.com>
2026-01-28 14:47:19 +0100

    tester

--- out/Test/FirstTest.java
+++ out/Test/FirstTest.java
@@ -1,10 +1,35 @@
 import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FirstTest {
+    Saab95 saab = new Saab95();
+    Volvo240 volvo = new Volvo240();
+
     @Test
-    public void nrDoorsSaab() {
-        Saab95 saab = new Saab95();
-        Assert.assertTrue(saab.getNrDoors() == 3);
+    public void TestGetNrDoorsSaab() {
+        Assert.assertTrue(saab.getNrDoors() == 2);
+    }
+    @Test
+    public void TestGetNrDoorsVolvo() {
+        Assert.assertTrue(volvo.getNrDoors() == 4);
+    }
+    @Test
+    public void TestEnginePowerSaab() {
+        Assert.assertTrue(saab.getEnginePower()==125);
+    }
+    @Test
+    public void TestEnginePowerVolvo() {
+        Assert.assertTrue(volvo.getEnginePower()==100);
+    }
+
+    @Test
+    public void TestGas() {
+        saab.startEngine();
+        double speedBefore = saab.getCurrentSpeed();
+        saab.gas(1);
+        Assert.assertTrue(saab.getCurrentSpeed()>speedBefore);
+        saab.startEngine();
     }
 }
