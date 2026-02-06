d68119015d6457428b9a9be727ba376ff65ab82c
axelnygren <s.axel.nygren@gmail.com>
2026-02-06 09:49:38 +0100

    Loadable

--- Scania.java
+++ Scania.java
@@ -1,12 +1,15 @@
 import java.awt.*;
+import java.util.ArrayList;
 
-public class Scania<T> extends Car {
+public class Scania<T> extends Car implements Loadable<T>{
 
+    ArrayList<T> last;
     private int flakTilt;
 
     // KONSTRUKTOR
     public Scania() {
         super(2,100, Color.blue, "Scania", 1000, 260);
+        last = new ArrayList<>();
         flakTilt = 0;
     }
 
@@ -21,5 +24,16 @@ public class Scania<T> extends Car {
     @Override
     public void move() {
         if (flakTilt ==0) super.move();
+    }
+
+    @Override
+    public void load(T t) {
+        last.add(t);
+    }
+
+    @Override
+    public T unload() {
+        if (last.isEmpty()) return null;
+        else return last.removeFirst();
     }
 }
