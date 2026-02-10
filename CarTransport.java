64b765972aa6188e090196079072a03a30562749
axelnygren <s.axel.nygren@gmail.com>
2026-02-10 14:07:40 +0100

    load, unload, follow update

--- CarTransport.java
+++ CarTransport.java
@@ -12,10 +12,10 @@ public class CarTransport extends Car implements Loadable<Car> {
     }
 
     public void trailerUp() {
-        if(!trailerIsUp) trailerIsUp = true;
+        trailerIsUp = true;
     }
     public void trailerDown() {
-        if (trailerIsUp) trailerIsUp = false;
+        if (getCurrentSpeed()==0) trailerIsUp = false;
     }
 
     @Override
@@ -32,14 +32,14 @@ public class CarTransport extends Car implements Loadable<Car> {
         double diffX =  (Math.abs(getPosition()[0] - c.getPosition()[0]));
         double diffY =  (Math.abs(getPosition()[1] - c.getPosition()[1]));
         return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY,2));
-
     }
 
     @Override
     public void load(Car c) {
-        if (carDistance(c) < 7) {
+        if (carDistance(c) < 7 && !trailerIsUp) {
             if ((nrLoadedCars < load.length) && (c.getClass() != CarTransport.class)) {
-                if (c.getWidth() < (getWidth() - 20)) {
+                if (c.getWidth() < (getWidth() - 20)
+                        && c.getLength() < 600) {
                     load[nrLoadedCars] = c;
                     nrLoadedCars++;
                     c.attachCar();
@@ -60,40 +60,11 @@ public class CarTransport extends Car implements Loadable<Car> {
         Car c = null;
         if ((!trailerIsUp) && (nrLoadedCars > 0)) {
                 c = load[nrLoadedCars - 1];
-                unloadmove(c);
+                c.reverse();
                 load[nrLoadedCars - 1] = null;
                 nrLoadedCars--;
                 c.deAttachCar();
             }
         return c;
     } //unload method
-
-    /*
-    public void unloadmove(Car c) {
-        double newX = 0;
-        double newY = 0;
-        double xCoord = getPosition()[0];
-        double yCoord = getPosition()[1];
-        int dir = getDirection();
-
-        if (dir == 2) {
-            newX = getPosition()[0] - (c.getLength() / 2 + getLength() / 2);
-            newY = yCoord;
-        } else if (dir == 0) {
-            newX = getPosition()[0] + (c.getLength() / 2 + getLength() / 2);
-            newY = yCoord;
-        } else if (dir == 1) {
-            newY = getPosition()[1] - (c.getLength() / 2 + getLength() / 2);
-            newX = xCoord;
-        } else if (dir == 3) {
-            newY = getPosition()[1] + (c.getLength() / 2 + getLength() / 2);
-            newX = xCoord;
-        }
-        Double[] pos = {newX, newY};
-        c.setPosition(pos);
-
-     */
-
-    } //unloadmove
-
 } //class carTransport
